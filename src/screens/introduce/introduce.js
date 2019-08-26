efed93c338479fc17122e17591aae6fc8c72b1e0
sunshine <sunshine@gmail.com>
2019-08-26 19:00:43 +0800

    init commit

--- src/screens/introduce/introduce.js
+++ src/screens/introduce/introduce.js
@@ -9,7 +9,6 @@
 import React, { Component } from 'react';
 import {
   SafeAreaView,
-  StyleSheet,
   ScrollView,
   View,
   Text,
@@ -18,7 +17,6 @@ import {
 import {
   Header,
   LearnMoreLinks,
-  Colors,
   DebugInstructions,
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
@@ -26,6 +24,7 @@ import {
 import {
   NavigationBar,
 } from '../../components';
+import styles from './introduce.styles';
 
 type Props = {};
 export default class IntroduceScreen extends Component<Props> {
@@ -62,7 +61,7 @@ export default class IntroduceScreen extends Component<Props> {
             style={styles.scrollView}
           >
             <Header />
-            {global.HermesInternal == null ? null : (
+            {global.HermesInternal === null ? null : (
               <View style={styles.engine}>
                 <Text style={styles.footer}>Engine: Hermes</Text>
               </View>
@@ -105,46 +104,3 @@ export default class IntroduceScreen extends Component<Props> {
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#ffffff',
-  },
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
